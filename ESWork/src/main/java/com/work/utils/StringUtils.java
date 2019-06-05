52052c94a57505a054a49291a8d8269052326849
zhourui <zhourui@hzyushi.cn>
2019-06-05 14:40:43 +0800

    添加StringUtils工具类

---
+++ ESWork/src/main/java/com/work/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.work.utils;
+
+/**
+ * @author zhourui
+ */
+public class StringUtils{
+    public static void main(String[] args) {
+        System.err.println("my name is zhou rui");
+    }
+}
+
